19e02fab6c33ce7b8d96735d6aeac2ea94232a98
chenfh5 <chenfh5@qq.com>
2018-11-08 23:54:01 +0800

    ADD: add leetcode string 20,522,556

---
+++ src/main/scala/io.github.chenfh5/lc/string/L0020_ValidParentheses.scala
@@ -0,0 +1,26 @@
+package io.github.chenfh5.lc.string
+
+
+object L0020_ValidParentheses {
+
+  // @see https://leetcode.com/problems/valid-parentheses/discuss/9178/Short-java-solution
+  def isValid(s: String): Boolean = {
+    import scala.collection.mutable
+    if (s.length % 2 != 0) return false
+    val stack = new mutable.Stack[Char]()
+    for (c <- s) {
+      if (c == '(') stack.push(')')
+      else if (c == '[') stack.push(']')
+      else if (c == '{') stack.push('}')
+      else if (stack.isEmpty || stack.pop() != c) return false // only buckets char
+    }
+    stack.isEmpty
+  }
+
+  def main(args: Array[String]) {
+    val input = "{[()]}"
+    val res = isValid(input)
+    assert(res)
+  }
+
+}
